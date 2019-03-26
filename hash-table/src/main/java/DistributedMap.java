b7271cac1519ae7495fa8d15fa0adc61f206bbfc
Karol Bąk <bak.karolek@gmail.com>
2019-03-26 12:04:49 +0100

    Fix semicols

--- hash-table/src/main/java/DistributedMap.java
+++ hash-table/src/main/java/DistributedMap.java
@@ -13,27 +13,27 @@ public class DistributedMap implements SimpleStringMap {
     public boolean containsKey(String key) {
         synchronized (map) {
             return map.containsKey(key);
-        };
+        }
     }
 
     @Override
     public Integer get(String key) {
         synchronized (map) {
             return map.get(key);
-        };
+        }
     }
 
     @Override
     public void put(String key, Integer value) {
         synchronized (map) {
             map.put(key, value);
-        };
+        }
     }
 
     @Override
     public Integer remove(String key) {
         synchronized (map) {
-            map.remove(key);
-        };
+            return map.remove(key);
+        }
     }
 }
