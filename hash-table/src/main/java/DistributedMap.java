28544a9116002cdb81b2637e70a6fd4ff59e908d
Karol Bąk <bak.karolek@gmail.com>
2019-03-26 11:59:46 +0100

    First steps done

--- hash-table/src/main/java/DistributedMap.java
+++ hash-table/src/main/java/DistributedMap.java
@@ -1,22 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class DistributedMap implements SimpleStringMap {
+
+    private Map<String, Integer> map;
+
+    public DistributedMap() {
+        map = new HashMap<>();
+    }
 
     @Override
     public boolean containsKey(String key) {
-        return false;
+        synchronized (map) {
+            return map.containsKey(key);
+        };
     }
 
     @Override
     public Integer get(String key) {
-        return null;
+        synchronized (map) {
+            return map.get(key);
+        };
     }
 
     @Override
     public void put(String key, Integer value) {
-
+        synchronized (map) {
+            map.put(key, value);
+        };
     }
 
     @Override
     public Integer remove(String key) {
-        return null;
+        synchronized (map) {
+            map.remove(key);
+        };
     }
 }
