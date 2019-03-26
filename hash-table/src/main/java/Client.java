080088cf71a37fceb0defa23ba9e297a5721afa0
Karol Bąk <bak.karolek@gmail.com>
2019-03-26 13:25:29 +0100

    Works for one client
    Need to change protocol.  Look for err messages.

--- hash-table/src/main/java/Client.java
+++ hash-table/src/main/java/Client.java
@@ -1,11 +1,7 @@
-import org.jgroups.JChannel;
-import org.jgroups.Message;
 import org.jgroups.protocols.*;
-import org.jgroups.protocols.pbcast.GMS;
-import org.jgroups.protocols.pbcast.NAKACK2;
-import org.jgroups.protocols.pbcast.STABLE;
-import org.jgroups.stack.ProtocolStack;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 
 public class Client {
@@ -13,34 +9,40 @@ public class Client {
     public static void main(String[] args) throws Exception {
         new UDP().setValue("mcast_group_addr", InetAddress.getByName("230.100.200.0"));
 
-        JChannel channel = new JChannel(false);
+        DistributedMap map = new DistributedMap("OperationChannel");
 
-        ProtocolStack stack = new ProtocolStack();
-        channel.setProtocolStack(stack);
-        stack.addProtocol(new UDP())
-                .addProtocol(new PING())
-                .addProtocol(new MERGE3())
-                .addProtocol(new FD_SOCK())
-                .addProtocol(new FD_ALL().setValue("timeout", 12000).setValue("interval", 3000))
-                .addProtocol(new VERIFY_SUSPECT())
-                .addProtocol(new BARRIER())
-                .addProtocol(new NAKACK2())
-                .addProtocol(new UNICAST3())
-                .addProtocol(new STABLE())
-                .addProtocol(new GMS())
-                .addProtocol(new UFC())
-                .addProtocol(new MFC())
-                .addProtocol(new FRAG2());
-
-        stack.init();
-        channel.connect("OperationChannel");
-
-        String line = "hello world";
-        Message msg = new Message(null, null, line);
-        channel.send(msg);
-
-        Thread.sleep(10000);
-        channel.close();
-
+        boolean running = true;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        while (running) {
+            System.out.print("> ");
+            System.out.flush();
+            String line = in.readLine().toLowerCase();
+            String[] tokens = line.split(" ");
+            switch(tokens[0]) {
+                case "exit":
+                case "quit":
+                    running = false;
+                    break;
+                case "get":
+                    Integer val = map.get(tokens[1]);
+                    System.out.println("  " + val);
+                    break;
+                case "put":
+                    map.put(tokens[1], Integer.parseInt(tokens[2]));
+                    break;
+                case "contains":
+                case "containsKey":
+                    Boolean result = map.containsKey(tokens[1]);
+                    System.out.println("  " + tokens[1] + " : " + result);
+                    break;
+                case "remove":
+                    map.remove(tokens[1]);
+                    break;
+                default:
+                    System.out.println("  error: unknown operation.");
+                    break;
+            }
+        }
+        map.delete();
     }
 }
--- hash-table/src/main/java/DistributedMap.java
+++ hash-table/src/main/java/DistributedMap.java
@@ -1,12 +1,68 @@
+import org.jgroups.JChannel;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.protocols.*;
+import org.jgroups.protocols.pbcast.GMS;
+import org.jgroups.protocols.pbcast.NAKACK2;
+import org.jgroups.protocols.pbcast.STABLE;
+import org.jgroups.stack.ProtocolStack;
+import org.jgroups.util.Util;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DistributedMap implements SimpleStringMap {
+public class DistributedMap extends ReceiverAdapter implements SimpleStringMap {
 
-    private Map<String, Integer> map;
+    private final Map<String, Integer> map;
+    private JChannel channel;
 
-    public DistributedMap() {
+    public DistributedMap(String cluster_name) throws Exception {
         map = new HashMap<>();
+        channel = new JChannel(false);
+
+        ProtocolStack stack = new ProtocolStack();
+        channel.setProtocolStack(stack);
+        stack
+                .addProtocol(new UDP())
+                .addProtocol(new PING())
+                .addProtocol(new MERGE3())
+                .addProtocol(new FD_SOCK())
+                .addProtocol(new FD_ALL().setValue("timeout", 12000).setValue("interval", 3000))
+                .addProtocol(new VERIFY_SUSPECT())
+                .addProtocol(new BARRIER())
+                .addProtocol(new NAKACK2())
+                .addProtocol(new UNICAST3())
+                .addProtocol(new STABLE())
+                .addProtocol(new GMS())
+                .addProtocol(new UFC())
+                .addProtocol(new MFC())
+                .addProtocol(new FRAG2());
+        stack.init();
+
+        System.out.println(" connecting to a channel...");
+        channel.connect(cluster_name, null, 0);
+    }
+
+    public void delete() {
+        System.out.println("  closing channel...");
+        channel.close();
+    }
+
+    public void getState(OutputStream output) throws Exception {
+        synchronized(map) {
+            Util.objectToStream(map, new DataOutputStream(output));
+        }
+    }
+
+    public void setState(InputStream input) throws Exception {
+        Map<String, Integer> new_map = (HashMap<String, Integer>) Util.objectFromStream(new DataInputStream(input));
+        synchronized(map) {
+            map.clear();
+            map.putAll(new_map);
+        }
     }
 
     @Override
@@ -27,13 +83,18 @@ public class DistributedMap implements SimpleStringMap {
     public void put(String key, Integer value) {
         synchronized (map) {
             map.put(key, value);
+            channel.getState();
         }
     }
 
     @Override
     public Integer remove(String key) {
         synchronized (map) {
-            return map.remove(key);
+            Integer result =  map.remove(key);
+            if (result != null) {
+                channel.getState();
+            }
+            return result;
         }
     }
 }
